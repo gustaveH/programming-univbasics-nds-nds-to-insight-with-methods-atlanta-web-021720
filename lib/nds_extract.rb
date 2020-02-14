023a6bf9f82d943d58f3eb50ca5bef94861d37ea
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 23:38:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,9 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  result = {}
+  i = 0 
+  
 
 end
 
